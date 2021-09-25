5abcf65e046552c20d77c17a0357643ae263faa0
Marcos S <45607262+MarcosNASA@users.noreply.github.com>
2021-09-25 10:38:58 +0200

    Create useComponentSelfRegistration.js

---
+++ useComponentSelfRegistration.js
@@ -0,0 +1,62 @@
+import { useLayoutEffect } from "react";
+
+export const useComponentSelfRegistration = ({
+  component,
+  useContext,
+  customIndex
+}) => {
+  const [registeredComponents, setRegisteredComponents] = useContext();
+
+  const index =
+    customIndex ??
+    registeredComponents.findIndex(
+      (registeredComponent) => component.element === registeredComponent.element
+    );
+
+  useLayoutEffect(() => {
+    const { element } = component;
+    const currentElement = element.current;
+    if (!currentElement) return;
+
+    setRegisteredComponents((previousRegisteredComponents) => {
+      return [
+        ...previousRegisteredComponents,
+        {
+          ...component,
+          index
+        }
+      ]
+        .sort(
+          (
+            { element: { current: a } = {} } = {},
+            { element: { current: b } = {} } = {}
+          ) => {
+            return a?.compareDocumentPosition(b) &
+              Node.DOCUMENT_POSITION_PRECEDING
+              ? 1
+              : -1;
+          }
+        )
+        .map((component, index) => ({
+          ...component,
+          index
+        }));
+    });
+
+    return () => {
+      setRegisteredComponents((previousRegisteredComponents) =>
+        previousRegisteredComponents.filter(
+          (registeredComponent) => registeredComponent.element === element
+        )
+      );
+    };
+  }, [
+    component,
+    index,
+    setRegisteredComponents,
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    ...Object.values(component)
+  ]);
+
+  return index;
+};
